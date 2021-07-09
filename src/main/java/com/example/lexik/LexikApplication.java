34c63a7dfce88636a329c4405677c6d160e8cd64
RigaAV <vasiljevs.a@inbox.lv>
2021-07-10 02:06:42 +0300

    Initial commit

---
+++ src/main/java/com/example/lexik/LexikApplication.java
@@ -0,0 +1,13 @@
+package com.example.lexik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LexikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LexikApplication.class, args);
+	}
+
+}
